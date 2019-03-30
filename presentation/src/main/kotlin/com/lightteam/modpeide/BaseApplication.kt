6a9e2ad52a77b291748ac795db66967cf1db9f0d
Dmitry Rubtsov <dm.mironov01@gmail.com>
2019-03-30 10:23:43 +0300

    Added new modules

---
+++ presentation/src/main/kotlin/com/lightteam/modpeide/BaseApplication.kt
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Light Team Software (Light Team) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The Light Team licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lightteam.modpeide
+
+import com.lightteam.modpeide.internal.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class BaseApplication : DaggerApplication() {
+
+    companion object PackageType {
+        const val PACKAGE_STANDARD = "com.KillerBLS.modpeide"
+        const val PACKAGE_ULTIMATE = "com.LightTeam.modpeidepro"
+    }
+
+    var isUltimate: Boolean = false
+
+    override fun onCreate() {
+        super.onCreate()
+        isUltimate = when (packageName) {
+            PACKAGE_STANDARD -> false
+            PACKAGE_ULTIMATE -> true
+            else -> false
+        }
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        val appComponent = DaggerAppComponent
+                .builder()
+                .application(this)
+                .build()
+        appComponent.inject(this)
+        return appComponent
+    }
+}
---
+++ presentation/src/main/kotlin/com/lightteam/modpeide/internal/di/AppComponent.kt
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Light Team Software (Light Team) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The Light Team licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lightteam.modpeide.internal.di
+
+import com.lightteam.modpeide.BaseApplication
+import com.lightteam.modpeide.internal.di.scopes.PerApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+@PerApplication
+@Component(modules = [
+    AndroidSupportInjectionModule::class,
+    AppModule::class,
+    ActivityBuilder::class
+])
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: BaseApplication): Builder
+        fun build(): AppComponent
+    }
+
+    override fun inject(instance: BaseApplication)
+}
