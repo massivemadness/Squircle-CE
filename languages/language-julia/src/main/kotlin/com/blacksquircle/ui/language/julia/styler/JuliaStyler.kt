50919a6c0d1ac9988f444e962968be695ec8c531
likewu <likewu@126.com>
2021-10-01 10:50:44 +0800

    Implement Julia language

---
+++ languages/language-julia/src/main/kotlin/com/blacksquircle/ui/language/julia/styler/JuliaStyler.kt
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2021 Squircle IDE contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blacksquircle.ui.language.julia.styler
+
+import android.util.Log
+import com.blacksquircle.ui.language.base.model.SyntaxScheme
+import com.blacksquircle.ui.language.base.span.StyleSpan
+import com.blacksquircle.ui.language.base.span.SyntaxHighlightSpan
+import com.blacksquircle.ui.language.base.styler.LanguageStyler
+import com.blacksquircle.ui.language.base.utils.StylingResult
+import com.blacksquircle.ui.language.base.utils.StylingTask
+import com.blacksquircle.ui.language.julia.lexer.JuliaLexer
+import com.blacksquircle.ui.language.julia.lexer.JuliaToken
+import java.io.IOException
+import java.io.StringReader
+import java.util.regex.Pattern
+
+class JuliaStyler private constructor() : LanguageStyler {
+
+    companion object {
+
+        private const val TAG = "JuliaStyler"
+
+        private val METHOD = Pattern.compile("(?<=(function)) (\\w+)")
+
+        private var juliaStyler: JuliaStyler? = null
+
+        fun getInstance(): JuliaStyler {
+            return juliaStyler ?: JuliaStyler().also {
+                juliaStyler = it
+            }
+        }
+    }
+
+    private var task: StylingTask? = null
+
+    override fun execute(sourceCode: String, syntaxScheme: SyntaxScheme): List<SyntaxHighlightSpan> {
+        val syntaxHighlightSpans = mutableListOf<SyntaxHighlightSpan>()
+        val sourceReader = StringReader(sourceCode)
+        val lexer = JuliaLexer(sourceReader)
+
+        // FIXME flex doesn't support positive lookbehind
+        val matcher = METHOD.matcher(sourceCode)
+        matcher.region(0, sourceCode.length)
+        while (matcher.find()) {
+            val styleSpan = StyleSpan(syntaxScheme.methodColor)
+            val syntaxHighlightSpan = SyntaxHighlightSpan(styleSpan, matcher.start(), matcher.end())
+            syntaxHighlightSpans.add(syntaxHighlightSpan)
+        }
+
+        while (true) {
+            try {
+                when (lexer.advance()) {
+                    JuliaToken.INTEGER_LITERAL,
+                    JuliaToken.FLOAT_LITERAL,
+                    JuliaToken.DOUBLE_LITERAL -> {
+                        val styleSpan = StyleSpan(syntaxScheme.numberColor)
+                        val syntaxHighlightSpan = SyntaxHighlightSpan(styleSpan, lexer.tokenStart, lexer.tokenEnd)
+                        syntaxHighlightSpans.add(syntaxHighlightSpan)
+                    }
+                    JuliaToken.SYMB_OP,
+                    JuliaToken.LONG_OP -> {
+                        val styleSpan = StyleSpan(syntaxScheme.operatorColor)
+                        val syntaxHighlightSpan = SyntaxHighlightSpan(styleSpan, lexer.tokenStart, lexer.tokenEnd)
+                        syntaxHighlightSpans.add(syntaxHighlightSpan)
+                    }
+                    /*JuliaToken.SEMICOLON,
+                    JuliaToken.COMMA,
+                    JuliaToken.DOT -> {
+                        continue // skip
+                    }*/
+                    JuliaToken.KEYWORD_OTHER,
+                    JuliaToken.KEYWORD_CONTROL -> {
+                        val styleSpan = StyleSpan(syntaxScheme.keywordColor)
+                        val syntaxHighlightSpan = SyntaxHighlightSpan(styleSpan, lexer.tokenStart, lexer.tokenEnd)
+                        syntaxHighlightSpans.add(syntaxHighlightSpan)
+                    }
+                    JuliaToken.BASE_TYPES -> {
+                        val styleSpan = StyleSpan(syntaxScheme.typeColor)
+                        val syntaxHighlightSpan = SyntaxHighlightSpan(styleSpan, lexer.tokenStart, lexer.tokenEnd)
+                        syntaxHighlightSpans.add(syntaxHighlightSpan)
+                    }
+                    JuliaToken.CONSTANTS -> {
+                        val styleSpan = StyleSpan(syntaxScheme.langConstColor)
+                        val syntaxHighlightSpan = SyntaxHighlightSpan(styleSpan, lexer.tokenStart, lexer.tokenEnd)
+                        syntaxHighlightSpans.add(syntaxHighlightSpan)
+                    }
+                    JuliaToken.DOUBLE_QUOTED_STRING,
+                    JuliaToken.SINGLE_QUOTED_STRING,
+                    JuliaToken.LONG_DOUBLE_QUOTED_STRING,
+                    JuliaToken.SINGLE_BACKTICK_STRING -> {
+                        val styleSpan = StyleSpan(syntaxScheme.stringColor)
+                        val syntaxHighlightSpan = SyntaxHighlightSpan(styleSpan, lexer.tokenStart, lexer.tokenEnd)
+                        syntaxHighlightSpans.add(syntaxHighlightSpan)
+                    }
+                    JuliaToken.LINE_COMMENT,
+                    JuliaToken.BLOCK_COMMENT -> {
+                        val styleSpan = StyleSpan(syntaxScheme.commentColor)
+                        val syntaxHighlightSpan = SyntaxHighlightSpan(styleSpan, lexer.tokenStart, lexer.tokenEnd)
+                        syntaxHighlightSpans.add(syntaxHighlightSpan)
+                    }
+                    JuliaToken.IDENTIFIER,
+                    JuliaToken.WHITESPACE,
+                    JuliaToken.BAD_CHARACTER -> {
+                        continue
+                    }
+                    JuliaToken.EOF -> {
+                        break
+                    }
+                }
+            } catch (e: IOException) {
+                Log.e(TAG, e.message, e)
+                break
+            }
+        }
+        return syntaxHighlightSpans
+    }
+
+    override fun enqueue(sourceCode: String, syntaxScheme: SyntaxScheme, stylingResult: StylingResult) {
+        task?.cancelTask()
+        task = StylingTask(
+            doAsync = { execute(sourceCode, syntaxScheme) },
+            onSuccess = stylingResult
+        )
+        task?.executeTask()
+    }
+
+    override fun cancel() {
+        task?.cancelTask()
+        task = null
+    }
+}
