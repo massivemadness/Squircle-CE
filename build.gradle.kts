96678ae4d542a7d084ff1f613ff04a3131588f06
Dmitrii Rubtsov <dm.mironov01@gmail.com>
2023-11-19 16:37:49 +0200

    Update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.android.navigation) apply false
+    alias(libs.plugins.android.hilt) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.kotlin.kover)
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.navigation) apply false
 }
 
 subprojects {
