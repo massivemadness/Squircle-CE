178aea00be6ae46a44f653b6738e916d6fef9b19
Dmitrii Mironov <dm.mironov01@gmail.com>
2025-04-09 10:44:37 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,44 @@ subprojects {
     }
 }
 
+val ktlint: Configuration by configurations.creating
+
+dependencies {
+    ktlint(libs.pinterest.ktlint) {
+        attributes {
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        }
+    }
+}
+
+tasks.register<JavaExec>("ktlintCheck") {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    args(
+        "**/src/**/*.kt",
+        "**.kts",
+        "!**/build/**",
+        "!sora-editor/**" // Exclude submodule from ktlint
+    )
+}
+
+tasks.register<JavaExec>("ktlintFormat") {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
+    args(
+        "-F",
+        "**/src/**/*.kt",
+        "**.kts",
+        "!**/build/**",
+        "!sora-editor/**" // Exclude submodule from ktlint
+    )
+}
+
 fun KoverProjectExtension.applyRules() {
     reports {
         filters {
@@ -102,42 +140,4 @@ fun KoverProjectExtension.applyRules() {
             }
         }
     }
-}
-
-val ktlint: Configuration by configurations.creating
-
-dependencies {
-    ktlint(libs.pinterest.ktlint) {
-        attributes {
-            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
-        }
-    }
-}
-
-tasks.register<JavaExec>("ktlintCheck") {
-    group = LifecycleBasePlugin.VERIFICATION_GROUP
-    description = "Check Kotlin code style."
-    classpath = ktlint
-    mainClass.set("com.pinterest.ktlint.Main")
-    args(
-        "**/src/**/*.kt",
-        "**.kts",
-        "!**/build/**",
-        "!sora-editor/**" // Exclude submodule from ktlint
-    )
-}
-
-tasks.register<JavaExec>("ktlintFormat") {
-    group = LifecycleBasePlugin.VERIFICATION_GROUP
-    description = "Fix Kotlin code style deviations."
-    classpath = ktlint
-    mainClass.set("com.pinterest.ktlint.Main")
-    jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
-    args(
-        "-F",
-        "**/src/**/*.kt",
-        "**.kts",
-        "!**/build/**",
-        "!sora-editor/**" // Exclude submodule from ktlint
-    )
 }
