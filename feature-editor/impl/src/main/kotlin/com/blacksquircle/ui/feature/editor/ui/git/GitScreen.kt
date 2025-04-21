1653aaf771fdee66b94e990b18680bb661cb5461
Andro_Dev <andromaster436@gmail.com>
2025-04-21 18:29:15 +0300

    crash fix

--- feature-editor/impl/src/main/kotlin/com/blacksquircle/ui/feature/editor/ui/git/GitScreen.kt
+++ feature-editor/impl/src/main/kotlin/com/blacksquircle/ui/feature/editor/ui/git/GitScreen.kt
@@ -23,20 +23,7 @@ import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.blacksquircle.ui.ds.dialog.AlertDialog
 import androidx.compose.ui.res.stringResource
 import com.blacksquircle.ui.ds.PreviewBackground
-import com.blacksquircle.ui.ds.SquircleTheme
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Text
-import androidx.compose.material.Icon
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.ui.res.painterResource
-import com.blacksquircle.ui.ds.R as UiR
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 
 @Composable
 internal fun GitScreen(
@@ -59,23 +46,8 @@ private fun GitScreen(
     AlertDialog(
         title = "Git",
         content = {
-            Column {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { /* ничего не делаем пока */ }
-                        .padding(16.dp)
-                ) {
-                    Icon(
-                        painter = painterResource(id = UiR.drawable.ic_git),
-                        contentDescription = null
-                    )
-                    Spacer(modifier = Modifier.width(16.dp))
-                    Column {
-                        Text(text = "Test", style = SquircleTheme.typography.text18Regular)
-                        Text(text = "Test button", style = SquircleTheme.typography.text18Regular)
-                    }
-                }
+            LazyColumn {
+                // todo
             }
         },
         dismissButton = stringResource(android.R.string.cancel),
