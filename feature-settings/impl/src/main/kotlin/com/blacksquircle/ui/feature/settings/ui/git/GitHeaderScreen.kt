af9b3c048ac0b933a9cb7d12c90bbed26a7040f8
Andro_Dev <andromaster436@gmail.com>
2025-04-22 21:13:28 +0300

    fixes

--- feature-settings/impl/src/main/kotlin/com/blacksquircle/ui/feature/settings/ui/git/GitHeaderScreen.kt
+++ feature-settings/impl/src/main/kotlin/com/blacksquircle/ui/feature/settings/ui/git/GitHeaderScreen.kt
@@ -51,7 +51,7 @@ internal fun GitHeaderScreen(
         GitHeaderViewModel.Factory().also(component::inject)
     }
 ) {
-    val viewState = viewModel.viewState.collectAsStateWithLifecycle().value
+    val viewState by viewModel.viewState.collectAsStateWithLifecycle()
     GitHeaderScreen(
         viewState = viewState,
         onBackClicked = viewModel::onBackClicked,
