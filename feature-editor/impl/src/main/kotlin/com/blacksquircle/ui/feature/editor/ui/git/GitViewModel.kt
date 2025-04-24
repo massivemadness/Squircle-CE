8799c5ba684c5bead18b1f37b6601f00acf759e7
Andro_Dev <andromaster436@gmail.com>
2025-04-24 11:01:19 +0300

    compile fix

--- feature-editor/impl/src/main/kotlin/com/blacksquircle/ui/feature/editor/ui/git/GitViewModel.kt
+++ feature-editor/impl/src/main/kotlin/com/blacksquircle/ui/feature/editor/ui/git/GitViewModel.kt
@@ -30,6 +30,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
