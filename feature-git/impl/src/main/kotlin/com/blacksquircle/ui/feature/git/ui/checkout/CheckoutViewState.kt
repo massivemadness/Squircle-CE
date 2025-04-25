fb830308f9950fc1937dee1465923b951066187c
Andro_Dev <andromaster436@gmail.com>
2025-04-25 17:07:17 +0300

    compile fix

--- feature-git/impl/src/main/kotlin/com/blacksquircle/ui/feature/git/ui/checkout/CheckoutViewState.kt
+++ feature-git/impl/src/main/kotlin/com/blacksquircle/ui/feature/git/ui/checkout/CheckoutViewState.kt
@@ -25,7 +25,7 @@ internal data class CheckoutViewState(
     val errorMessage: String = "",
     val showBranchInput: Boolean = false,
     val checkoutBranch: String = "",
-    val checkoutBranches: List<String> = null,
+    val checkoutBranches: List<String> = emptyList(),
     val showListOfBranches: Boolean = false
 ) : ViewState {
 
