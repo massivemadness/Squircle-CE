430c8e0bc30ef3cb7b5f1053f9527e1ef51838ec
Dmitrii Mironov <dm.mironov01@gmail.com>
2025-04-27 13:28:54 +0300

    Clone repository

--- feature-git/impl/src/main/kotlin/com/blacksquircle/ui/feature/git/data/interactor/GitInteractorImpl.kt
+++ feature-git/impl/src/main/kotlin/com/blacksquircle/ui/feature/git/data/interactor/GitInteractorImpl.kt
@@ -22,11 +22,16 @@ import com.blacksquircle.ui.feature.git.api.exception.InvalidCredentialsExceptio
 import com.blacksquircle.ui.feature.git.api.exception.RepositoryNotFoundException
 import com.blacksquircle.ui.feature.git.api.exception.UnsupportedFilesystemException
 import com.blacksquircle.ui.feature.git.api.interactor.GitInteractor
+import com.blacksquircle.ui.filesystem.base.exception.FileNotFoundException
 import com.blacksquircle.ui.filesystem.base.model.FileModel
 import com.blacksquircle.ui.filesystem.local.LocalFilesystem
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.withContext
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.ProgressMonitor
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
 import java.io.File
 
 internal class GitInteractorImpl(
@@ -62,7 +67,50 @@ internal class GitInteractorImpl(
     }
 
     override suspend fun cloneRepository(fileModel: FileModel, url: String): Flow<String> {
-        return emptyFlow()
+        return callbackFlow {
+            if (fileModel.filesystemUuid != LocalFilesystem.LOCAL_UUID) {
+                throw UnsupportedFilesystemException()
+            }
+
+            if (settingsManager.gitCredentialsUsername.isBlank() ||
+                settingsManager.gitCredentialsPassword.isBlank() ||
+                settingsManager.gitUserEmail.isBlank() ||
+                settingsManager.gitUserName.isBlank()
+            ) {
+                throw InvalidCredentialsException()
+            }
+
+            val file = File(fileModel.path)
+            if (!file.exists() || !file.isDirectory) {
+                throw FileNotFoundException(fileModel.path)
+            }
+
+            val credentialsProvider = UsernamePasswordCredentialsProvider(
+                settingsManager.gitCredentialsUsername,
+                settingsManager.gitCredentialsPassword
+            )
+
+            Git.cloneRepository()
+                .setURI(url)
+                .setDirectory(file)
+                .setCredentialsProvider(credentialsProvider)
+                .setProgressMonitor(object : ProgressMonitor {
+                    override fun start(totalTasks: Int) = Unit
+                    override fun beginTask(title: String?, totalWork: Int) {
+                        trySend(title.orEmpty())
+                    }
+                    override fun update(completed: Int) = Unit
+                    override fun endTask() {
+                        close()
+                    }
+                    override fun isCancelled(): Boolean {
+                        return false
+                    }
+                })
+                .call()
+
+            awaitClose()
+        }
     }
 
     companion object {
